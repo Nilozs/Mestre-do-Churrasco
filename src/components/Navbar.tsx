8ff2b27f73e769a79daec26291d8f5313591dc55
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-08-24 18:32:48 -0300

    feat: create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react"
+import React from "react"
+
+function Example() {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Cabeçalho</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      {/* <IonContent className="ion-padding">
+        <h1>Content</h1>
+      </IonContent> */}
+    </>
+  )
+}
+export default Example
