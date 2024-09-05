204fc8c6518858b9ee79d0b5ba36239baa7bfbb4
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-09-04 21:02:30 -0300

    style: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-dark': '#0D0D0D',
+        'custom-maroon': '#731702',
+        'custom-red': '#F23005',
+        'custom-orange': '#F27B13',
+        'custom-yellow': '#F2A81D',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
