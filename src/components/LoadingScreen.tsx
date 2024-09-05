aea28920466374905d5a9d57ea9011e149f3992d
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-09-04 21:02:59 -0300

    refactor: styles from splash screen and add font

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,15 +1,18 @@
-import { useIonRouter } from "@ionic/react"
+import { IonIcon, useIonRouter } from "@ionic/react"
+import { logoFacebook, logoInstagram, logoTwitter } from "ionicons/icons"
 import React, { useEffect } from "react"
+import { useAuth } from "../context/middleware"
 import "./LoadingScreen.css"
 
 const LoadingScreen: React.FC = () => {
   const router = useIonRouter()
+  const { state } = useAuth()
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      const user = localStorage.getItem("user")
+      if (state.loading) return
 
-      if (user) {
+      if (state.isAuthenticated) {
         router.push("/home")
       } else {
         router.push("/auth")
@@ -17,12 +20,15 @@ const LoadingScreen: React.FC = () => {
     }, 2000)
 
     return () => clearTimeout(timer)
-  }, [router])
+  }, [state, router])
 
   return (
-    <div className="loading-container transition">
-      <img src="/logo.png" alt="logo-app" />
-      <p className="loading-text">Gril Real</p>
+    <div className="loading-container">
+      <img src="/image-loadscreen.jpg" alt="logo-app" />
+      <div className="logo-section">
+        <img src="/logo2.png" alt="logo-app" width={60} height={60} />
+        <h2 className="loading-text font-sans mb-10 text-4xl">Grill Real</h2>
+      </div>
     </div>
   )
 }
