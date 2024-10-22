7ea1638b621a513ffc8d969a45c88145057314e7
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-22 14:25:39 -0300

    feat: remove pages

--- src/pages/ChurrasPage.tsx
+++ src/pages/ChurrasPage.tsx
@@ -1,7 +1,6 @@
 import { MaxWidthWrapper } from "@/components/animation"
 import InputsApi from "@/components/churascometro/inputs-api"
 import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react"
-import ChurrascometroForm from "../components/churascometro/multi-step-form"
 
 const ChurrasPage = () => (
   <>
