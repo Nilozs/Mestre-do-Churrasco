a64acbd90adbb9a1faf3000e80c77493338a51de
Nilozs <144507366+Nilozs@users.noreply.github.com>
2024-11-27 05:35:28 -0300

    feat: adicionando botão nos processadores da home

--- src/data/steakhouse.ts
+++ src/data/steakhouse.ts
@@ -6,6 +6,7 @@ export const churrascariasCampoGrande = [
     descricao:
       "O AMD Ryzen 9 7950X é uma potência com 16 núcleos e 32 threads, ideal para gamers, criadores de conteúdo e multitarefas pesadas. Oferece desempenho extremo e suporte para DDR5 e PCIe 5.0.",
     top: 1,
+    link: "https://www.amd.com/pt/products/processors/desktops/ryzen/7000-series/amd-ryzen-9-7950x.html",
   },
   {
     id: 2,
@@ -14,6 +15,7 @@ export const churrascariasCampoGrande = [
     descricao:
       "O Intel Core i9-13900K, com 24 núcleos (8 de desempenho e 16 de eficiência), é perfeito para gamers hardcore e profissionais que buscam máxima performance. Compatível com DDR5 e PCIe 5.0.",
     top: 2,
+    link: "https://www.intel.com.br/content/www/br/pt/products/sku/230496/intel-core-i913900k-processor-36m-cache-up-to-5-80-ghz/specifications.html",
   },
   {
     id: 3,
@@ -22,6 +24,7 @@ export const churrascariasCampoGrande = [
     descricao:
       "Com a tecnologia 3D V-Cache, o Ryzen 7 7800X3D é um dos melhores processadores para jogos. Possui 8 núcleos, 16 threads e clock de alto desempenho para experiências imersivas.",
     top: 3,
+    link: "https://www.amd.com/pt/products/processors/desktops/ryzen/7000-series/amd-ryzen-7-7800x3d.html",
   },
   {
     id: 4,
@@ -30,6 +33,7 @@ export const churrascariasCampoGrande = [
     descricao:
       "Equilibrando custo e desempenho, o Core i7-13700K oferece 16 núcleos (8 de desempenho e 8 de eficiência) e é excelente para jogos e multitarefa. Compatível com as mais recentes tecnologias.",
     top: 4,
+    link: "https://www.intel.com.br/content/www/br/pt/products/sku/230500/intel-core-i713700k-processor-30m-cache-up-to-5-40-ghz/specifications.html",
   },
   {
     id: 5,
@@ -38,6 +42,7 @@ export const churrascariasCampoGrande = [
     descricao:
       "Ideal para gamers que buscam alta performance a um custo acessível, o Ryzen 5 7600X possui 6 núcleos e 12 threads, além de suporte a DDR5 e PCIe 5.0.",
     top: 5,
+    link: "https://www.amd.com/pt/products/processors/desktops/ryzen/7000-series/amd-ryzen-5-7600x.html",
   },
   {
     id: 6,
@@ -46,5 +51,6 @@ export const churrascariasCampoGrande = [
     descricao:
       "Com 14 núcleos (6 de desempenho e 8 de eficiência), o Intel Core i5-13600K entrega excelente custo-benefício, sendo ideal para jogos e produtividade.",
     top: 6,
+    link: "https://www.intel.com.br/content/www/br/pt/products/sku/230493/intel-core-i513600k-processor-24m-cache-up-to-5-10-ghz/specifications.html",
   },
 ];
